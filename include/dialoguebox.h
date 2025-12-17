640631b5c56ef7c51a8ddf60299715a3c858fb6e
Katie Buell <katelynbuell55@gmail.com>
2025-12-16 21:39:51 -0500

    remove unused dialogue code

--- include/dialoguebox.h
+++ include/dialoguebox.h
@@ -11,7 +11,6 @@
 #include "bn_sprite_text_generator.h"
 #include "bn_unique_ptr.h"
 
-// #include "common_variable_8x16_sprite_font.h"
 #include "temp_font_variable_font.h"
 
 #include "bn_regular_bg_items_dialogue_box.h"
@@ -124,58 +123,10 @@ namespace kt {
                 }
             };
 
-            void update_diaogue() {
-                // line_1_sprites[curr_char].set_y(line_1_y_pos);
-                // curr_char++;
-            }
-
-            // void set_speaker(bn::string<16> new_speaker) {
-            //     // speaker = new_speaker;
-            // };
-
-            // void set_text(bn::string<128> new_text) {
-            //     // text = new_text;
-            //     prepare_text();
-            // };
-
-            // void set_map(bn::vector<bn::pair<bn::string_view, bn::string_view>, 24> new_map) {
-            //     // Empty old map (if any)
-            //     dialogue_map.clear();
-                
-            //     // Add new map, splitting long text into multiple lines
-            //     for (int i = 0; i < new_map.size(); i++) {
-            //         int length = 0;
-            //         bn::string<128> curr_line = "";
-            //         for (int j = 0; j < new_map[i].second.length(); j++) {
-            //             // Check for space
-            //             if (new_map[i].second[j] == ' ') {
-            //                 // Determine if need to split
-            //                 if (length + length_of_word(new_map[i].second, j) > line_width) {
-            //                     // Save the current line
-            //                     // dialogue_map.push_back(bn::make_pair<bn::string_view, bn::string_view>(bn::to_string<16>(new_map[i].first), curr_line));
-            //                     curr_line = "";
-            //                     length = 0;
-            //                 } else {
-            //                     curr_line.push_back(new_map[i].second[j]);
-            //                     length += temp_font_variable_font_character_widths[new_map[i].second[j] - 32];
-            //                 }
-            //             } else {
-            //                 curr_line.push_back(new_map[i].second[j]);
-            //                 length += temp_font_variable_font_character_widths[new_map[i].second[j] - 32];
-            //             }
-            //         }
-            //     }
-
-            //     // DEBUG print map
-            //     for (int i = 0; i < dialogue_map.size(); i++) {
-            //         bn::log(bn::to_string<16>(dialogue_map[i].first));
-            //         bn::log(bn::to_string<128>(dialogue_map[i].second));
-            //     }
-            // }
-
             auto prio() {
                 return bg.priority();
             };
+
         private:
             void start_dialogue() {
                 for (bn::sprite_ptr& line_1_sprite : line_1_sprites) {
@@ -196,14 +147,6 @@ namespace kt {
                     bn::core::update();
                 }
             };
-
-            int length_of_word(bn::string<128> dialogue, int start) {
-                int length = 0;
-                for (int i = start; dialogue[i] != ' ' && i < dialogue.length(); i++) {
-                    length += temp_font_variable_font_character_widths[dialogue[i] - 32];
-                }
-                return length;
-            };
             
             bn::regular_bg_ptr bg;
             bn::sprite_text_generator line_1;
@@ -216,18 +159,9 @@ namespace kt {
             bn::vector<bn::sprite_ptr, 16> nameplate_sprites;
             bn::sprite_ptr port_spr;
 
-            bn::vector<bn::pair<bn::string_view, bn::string_view>, 24> dialogue_map;
-
             bool showing = false;
             bool typing = false;
-            // int char_width = 6;
-            // int char_height = 16;
-            // int line_width = 200;
             uint8_t num_pages = 1;
             uint8_t curr_page = 0;
-            uint8_t curr_char = 0;
-
-            // bn::string<16> speaker = "default";
-            // bn::string<128> text = "default text";
     };
 }
